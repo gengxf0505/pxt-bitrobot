58458e7a980aaeaa1003e0db17318fd6f7fd6179
fingermaker <xiang.jin@qq.com>
2017-10-29 16:00:26 +0800

    xiangjin
    测试汉化界面

--- main.ts
+++ main.ts
@@ -3,7 +3,7 @@
   * Enumeration of motors.
   */
 enum BBMotor {
-    //% block="left"
+    //% block="左"
     Left,
     //% block="right"
     Right,
@@ -78,7 +78,7 @@ namespace bitbot {
         if (speed > 1023) {
             speed = 1023;
         } else if (speed < -1023) {
-            speed = 1023;
+            speed = -1023;
         }
 
         let realSpeed = speed;
